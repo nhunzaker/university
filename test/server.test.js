3f2c7fc9d638ca46f6dac9374d587d07efa616aa
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-12-18 08:06:13 -0500

    Add linting, format tests

--- test/server.test.js
+++ test/server.test.js
@@ -4,11 +4,11 @@ var assert = require('assert')
 describe('When Server.init is called at a given port', function() {
   var PORT = 9000
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     this.server = Server.init(PORT, done)
   })
 
-  afterEach(function(done) {
+  afterEach(function (done) {
     this.server.stop(done)
   })
 
@@ -19,17 +19,17 @@ describe('When Server.init is called at a given port', function() {
   it ('serves the version of the project at /version', function (done) {
     // server.inject comes from the Shot testing library
     // https://github.com/hapijs/shot
-    this.server.inject('/version', function(response) {
+    this.server.inject('/version', function (response) {
       assert.equal(response.statusCode, 200)
       assert.deepEqual(response.result, Server.metadata)
       done()
     })
   })
 
-  context('when another server is created at that port', function() {
+  context('when another server is created at the same port', function() {
 
-    it ('returns an address in use error', function(done) {
-      Server.init(PORT, function(error, server) {
+    it ('returns an address in use error', function (done) {
+      Server.init(PORT, function (error, server) {
         assert.equal(error.code, 'EADDRINUSE')
         server.stop(done)
       })
