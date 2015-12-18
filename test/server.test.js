e2af8e3ce3085e2b4b78aa72e08b786882d43c4c
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-12-18 08:34:09 -0500

    Update reporting, add additional context

--- test/server.test.js
+++ test/server.test.js
@@ -1,40 +1,42 @@
 var Server = require('../lib/server')
 var assert = require('assert')
 
-describe('When Server.init is called at a given port', function() {
-  var PORT = 9000
+describe('Server', function() {
 
-  beforeEach(function (done) {
-    this.server = Server.init(PORT, done)
-  })
+  context('When Server.init is called at a given port', function() {
+    var PORT = 9000
 
-  afterEach(function (done) {
-    this.server.stop(done)
-  })
-
-  it ('creates a server at a given port', function () {
-    assert.equal(this.server.info.port, PORT)
-  })
-
-  it ('serves the version of the project at /version', function (done) {
-    // server.inject comes from the Shot testing library
-    // https://github.com/hapijs/shot
-    this.server.inject('/version', function (response) {
-      assert.equal(response.statusCode, 200)
-      assert.deepEqual(response.result, Server.metadata)
-      done()
+    beforeEach(function (done) {
+      this.server = Server.init(PORT, done)
     })
-  })
 
-  context('when another server is created at the same port', function() {
+    afterEach(function (done) {
+      this.server.stop(done)
+    })
 
-    it ('returns an address in use error', function (done) {
-      Server.init(PORT, function (error, server) {
-        assert.equal(error.code, 'EADDRINUSE')
-        server.stop(done)
+    it ('creates a server at a given port', function () {
+      assert.equal(this.server.info.port, PORT)
+    })
+
+    it ('serves the version of the project at /version', function (done) {
+      // server.inject comes from the Shot testing library
+      // https://github.com/hapijs/shot
+      this.server.inject('/version', function (response) {
+        assert.equal(response.statusCode, 200)
+        assert.deepEqual(response.result, Server.metadata)
+        done()
       })
     })
 
-  })
+    context('when another server is created at the same port', function() {
 
+      it ('signals an address in use error', function (done) {
+        Server.init(PORT, function (error, server) {
+          assert.equal(error.code, 'EADDRINUSE')
+          server.stop(done)
+        })
+      })
+
+    })
+  })
 })
